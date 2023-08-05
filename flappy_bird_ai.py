836b9c089ef3a25d36f3d0b7dc5ea6033ba5f9e8
Emeika <shahbazhafsa2014@gmail.com>
2023-08-06 00:40:15 +0500

    Function to get input index of pipes

--- flappy_bird_ai.py
+++ flappy_bird_ai.py
@@ -50,6 +50,16 @@ def welcomeScreen():
                 pygame.display.update()
                 FPSCLOCK.tick(FPS)
 
+#define a function to get the input index of the pipes
+def get_index(pipes, birds):
+    #get the birds' x position
+    bird_x = birds[0].x
+    #calculate the x distance between birds and each pipes
+    list_distance = [pipe.x + pipe.IMG_WIDTH - bird_x for pipe in pipes]
+    #get the index of the pipe that has the minimum non negative distance(the closest pipe in front of the bird)
+    index = list_distance.index(min(i for i in list_distance if i >= 0)) 
+    return index
+
 def mainGame(config, genomes):
     global generation, SCREEN #use the global variable gen and SCREEN
     screen = SCREEN
